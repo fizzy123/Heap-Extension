70a3311d1a7c6ac01324e9ec5c366f4052efa1b1
Nobel Yoo <nobr@nobr.me>
2015-03-07 03:21:06 -0800

    Changed to update icon on tab switch
    Changed to update icon on tab switch

--- background.js
+++ background.js
@@ -26,18 +26,19 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 
 var extensionAction = function(){
   chrome.tabs.query({active:true, currentWindow:true}, function(tab){
-	$.get('http://192.168.62.127/csrf/', function(res) {
-		$.post('http://192.168.62.127/feed/action/', 
-		   {pk:window.localStorage[tab[0].id],
-			url: tab[0].url},
-		   function(res) {
+	$.get('http://heap.nobr.me/csrf/', function(res) {
+	  $.post('http://heap.nobr.me/feed/action/', 
+		     {pk:window.localStorage[tab[0].id],
+			  url: tab[0].url},
+		      function(res) {
 		if (res.url) {
 		  if (res.url.match(/^http/)) {
 			chrome.tabs.update({url: res.url});
 		  } else {
-			chrome.tabs.create({url:'http://192.168.62.127' + res.url});
+			chrome.tabs.create({url:'http://heap.nobr.me' + res.url});
 		  }
 		}
+		checkUrl(tab[0].id, tab[0].url);
 	  })
 	})
   })
@@ -48,8 +49,6 @@ chrome.commands.getAll(function(data){
 })
 
 chrome.commands.onCommand.addListener(function(command){
-  console.log('extension action')
-  console.log(command)
   if (command === '_extension_action') {
     extensionAction();
   }
@@ -57,22 +56,32 @@ chrome.commands.onCommand.addListener(function(command){
 
 chrome.browserAction.onClicked.addListener(extensionAction)
 
+var checkUrl = function(tabId, url) {
+  $.get('http://heap.nobr.me/csrf/', function(res) {
+	csrftoken = res.match(/value=\'(.+)\'/)[1]
+	$.post('http://heap.nobr.me/feed/check_url/', 
+			 {url: url},
+			 function(res) {
+	  if (res.pk) {
+		window.localStorage[tabId] = res.pk
+		chrome.browserAction.setIcon({path: './assets/icons/check.png'})
+	  } else {
+		chrome.browserAction.setIcon({path: './assets/icons/add.png'})
+	  }
+	})	  
+  })
+}
+
 chrome.tabs.onUpdated.addListener(function(tabId, data){
   chrome.tabs.query({active:true, currentWindow:true}, function(tab) {
 	if (tabId === tab[0].id && data.status === 'loading' && data.url) {
-	  $.get('http://192.168.62.127/csrf/', function(res) {
-	    csrftoken = res.match(/value=\'(.+)\'/)[1]
-		$.post('http://192.168.62.127/feed/check_url/', 
-			   {url:data.url},
-			   function(res) {
-		  if (res.pk) {
-			window.localStorage[tabId] = res.pk
-			chrome.browserAction.setIcon({path: './assets/icons/check.png'})
-		  } else {
-			chrome.browserAction.setIcon({path: './assets/icons/add.png'})
-		  }
-		})	  
-	  })
+	  checkUrl(tabId, data.url)
 	}
   })
+})  
+
+chrome.tabs.onActivated.addListener(function() {
+  chrome.tabs.query({active:true, currentWindow:true}, function(tab) {
+	checkUrl(tab[0].id, tab[0].url);
+  });
 })
--- redirect.js
+++ redirect.js
@@ -1,8 +1,8 @@
-$.get('http://192.168.62.127/feed/redirect', function(res){
+$.get('http://heap.nobr.me/feed/redirect', function(res){
   if (res.url.slice(0,4) === 'http') {
     chrome.runtime.sendMessage({pk: res.pk}); 
 	window.location = res.url
   } else {
-     window.location = 'http://192.168.62.127' + res.url;
+     window.location = 'http://heap.nobr.me' + res.url;
   }
 })
